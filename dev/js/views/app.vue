ead15a4abe75be398cf152671b3cf54820790af6
zdz <1696498456@qq.com>
2016-10-19 09:34:53 +0800

    初始项目

---
+++ dev/js/views/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        主路由
+        <router-view ></router-view>
+    </div>
+</template>
+<script type="text/javascript">
+    export default {
+        data (){
+            return {}
+        },
+        ready (){
+        }
+    }
+</script>
